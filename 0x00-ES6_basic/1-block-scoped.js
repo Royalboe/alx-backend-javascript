004eb1b4fa66795faacada1ad6db55d7f2cd5381
Royalboe <auomidejohnson@gmail.com>
2022-12-19 20:17:36 +0100

    block scoped

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  var task;
+  var task2;
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+  task = false;
+  task2 = true;
+  return [task, task2];
+}
